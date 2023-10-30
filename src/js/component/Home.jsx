59b51eceb95efea73ce71d172c4491ea6aa5b544
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-10-30 21:22:19 +0000

    ok

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../component/Card";
+
+
+//create your first component
+const Home = () => {
+	return (
+		<div >
+			<Card/>
+		</div>
+	);
+};
+
+export default Home;
+
+ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
